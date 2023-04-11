4fa972e32f339b2d71b99e94f6e55eb58cc58af7
Erlyn Ascarate <erlyndavid@outlook.es>
2023-04-11 14:58:50 -0500

    Comienza el proyecto

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import { CssBaseline } from '@mui/material'
+
+const App = () => {
+    return (
+        <>
+            <CssBaseline />
+            <div>App</div>
+        </>
+    )
+}
+
+export default App
