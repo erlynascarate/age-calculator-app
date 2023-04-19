7f49a96624a809a968e439104ee874fc5ebd77fc
Erlyn Ascarate <erlyndavid@outlook.es>
2023-04-19 14:58:10 -0500

    Combierte la App en una PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,65 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+const description = `Age Culculate App
+
+Age icons created by Freepik - Flaticon
+https://www.flaticon.com/free-icons/age`
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            injectRegister: 'inline',
+            includeAssets: [
+                'favicon.ico',
+                'favicon-32x32.png',
+                'favicon-16x16.png',
+                'apple-touch-icon.png',
+            ],
+            manifest: {
+                name: 'Age Calculator',
+                short_name: 'Age',
+                description,
+                theme_color: '#ffffff',
+                background_color: '#ffffff',
+                orientation: 'portrait',
+                icons: [
+                    {
+                        src: 'pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'maskable_icon_x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'maskable',
+                    },
+                    {
+                        src: 'maskable_icon.png',
+                        sizes: '731x731',
+                        type: 'image/png',
+                        purpose: 'maskable',
+                    },
+                ],
+                // screenshots: [
+                //     {
+                //         src: 'screenshot.png',
+                //         sizes: '850x1860',
+                //         type: 'image/png',
+                //         platform: 'narrow',
+                //         label: 'Tip calculator being used',
+                //     },
+                // ],
+            },
+        }),
+    ],
 })
