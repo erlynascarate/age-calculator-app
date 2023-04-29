d05d00776bb156852111dab2bd43710e31c562e8
Erlyn Ascarate <erlyndavid@outlook.es>
2023-04-29 17:19:05 -0500

    Wrap DividerButton with Tooltip

--- src/components/FormDivider.jsx
+++ src/components/FormDivider.jsx
@@ -1,26 +1,27 @@
-import { Button, Divider } from '@mui/material'
+import { Button, Divider, Tooltip } from '@mui/material'
 
 const DividerButton = () => (
-    <Button
-        type='submit'
-        color='primary'
-        variant='contained'
-        title='Calculate Age'
-        sx={{
-            borderRadius: '50%',
-            inlineSize: '64px',
-            blockSize: '64px',
-            '&:hover': {
-                bgcolor: 'var(--offBlack)',
-            },
-        }}
-    >
-        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 46 44'>
-            <g fill='none' stroke='#FFF' strokeWidth='2'>
-                <path d='M1 22.019C8.333 21.686 23 25.616 23 44M23 44V0M45 22.019C37.667 21.686 23 25.616 23 44' />
-            </g>
-        </svg>
-    </Button>
+    <Tooltip title='Calculate Age'>
+        <Button
+            sx={{
+                borderRadius: '50%',
+                inlineSize: '64px',
+                blockSize: '64px',
+                '&:hover': {
+                    bgcolor: 'var(--offBlack)',
+                },
+            }}
+            color='primary'
+            type='submit'
+            variant='contained'
+        >
+            <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 46 44'>
+                <g fill='none' stroke='#FFF' strokeWidth='2'>
+                    <path d='M1 22.019C8.333 21.686 23 25.616 23 44M23 44V0M45 22.019C37.667 21.686 23 25.616 23 44' />
+                </g>
+            </svg>
+        </Button>
+    </Tooltip>
 )
 
 const FormDivider = () => {
@@ -35,8 +36,8 @@ const FormDivider = () => {
                 <DividerButton />
             </Divider>
             <Divider
-                textAlign='right'
                 sx={{ display: { xs: 'none', md: 'flex' } }}
+                textAlign='right'
             >
                 <DividerButton />
             </Divider>
