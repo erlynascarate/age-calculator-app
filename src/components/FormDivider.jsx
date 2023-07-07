7ecf25b087df26be818c63dc9e084649b8c1f2ca
Erlyn Ascarate <erlynascarate@gmail.com>
2023-07-07 17:09:19 -0500

    Order the code

--- src/components/FormDivider.jsx
+++ src/components/FormDivider.jsx
@@ -1,44 +1,48 @@
-import { Button, Divider, Tooltip } from '@mui/material'
-
-const DividerButton = () => (
-    <Tooltip title='Calculate Age'>
-        <Button
-            sx={{
-                borderRadius: '50%',
-                inlineSize: '64px',
-                blockSize: '64px',
-                '&:hover': {
-                    bgcolor: 'primary.hover',
-                },
-            }}
-            type='submit'
-            variant='contained'
-        >
-            <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 46 44'>
-                <g fill='none' stroke='#FFF' strokeWidth='2'>
-                    <path d='M1 22.019C8.333 21.686 23 25.616 23 44M23 44V0M45 22.019C37.667 21.686 23 25.616 23 44' />
-                </g>
-            </svg>
-        </Button>
-    </Tooltip>
-)
+import {
+    Button,
+    Divider,
+    Tooltip,
+    useMediaQuery,
+    useTheme,
+} from '@mui/material'
 
 const FormDivider = () => {
+    const theme = useTheme()
+    const md = useMediaQuery(theme.breakpoints.up('md'))
+
+    const textAlign = md ? 'right' : 'center'
+
     return (
         <>
             <Divider
                 sx={{
-                    display: { md: 'none' },
-                    marginBlock: '32px 24px',
+                    marginBlock: { xs: '32px 24px', md: 0 },
                 }}
+                textAlign={textAlign}
             >
-                <DividerButton />
-            </Divider>
-            <Divider
-                sx={{ display: { xs: 'none', md: 'flex' } }}
-                textAlign='right'
-            >
-                <DividerButton />
+                <Tooltip title='Calculate Age'>
+                    <Button
+                        sx={{
+                            borderRadius: '50%',
+                            inlineSize: '64px',
+                            blockSize: '64px',
+                            '&:hover': {
+                                bgcolor: 'primary.hover',
+                            },
+                        }}
+                        type='submit'
+                        variant='contained'
+                    >
+                        <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            viewBox='0 0 46 44'
+                        >
+                            <g fill='none' stroke='#FFF' strokeWidth='2'>
+                                <path d='M1 22.019C8.333 21.686 23 25.616 23 44M23 44V0M45 22.019C37.667 21.686 23 25.616 23 44' />
+                            </g>
+                        </svg>
+                    </Button>
+                </Tooltip>
             </Divider>
         </>
     )
